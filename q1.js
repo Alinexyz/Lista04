30a46f378b0e1d317b7e6b82e68a8c7efc1cc874
Alinexyz <av399292965@gmail.com>
2025-09-24 11:35:04 -0300

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+let idade = prompt("Digite sua idade:");
+idade = Number(idade);
+
+if (idade >= 0 && idade <= 12) {
+  console.log("Criança");
+} else if (idade >= 13 && idade <= 17) {
+  console.log("Adolescente");
+} else if (idade >= 18 && idade <= 59) {
+  console.log("Adulto");
+} else if (idade >= 60) {
+  console.log("Idoso");
+} else {
+  console.log("Idade inválida");
+}
